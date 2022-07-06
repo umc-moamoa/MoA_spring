14ee8553f2dbdf61929489c95ecf353aa8894fb1
Kim Miso <dutlsalth9811@gmail.com>
2022-07-06 19:38:13 +0900

    Add User post API

--- src/main/java/com/springboot/moa/user/UserDao.java
+++ src/main/java/com/springboot/moa/user/UserDao.java
@@ -1,6 +1,7 @@
 package com.springboot.moa.user;
 
 import com.springboot.moa.user.model.GetUserInfoRes;
+import com.springboot.moa.user.model.GetUserPostRes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -17,7 +18,7 @@ public class UserDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public List<GetUserInfoRes> selectUser(int userId){
+    public GetUserInfoRes selectUser(int userId){
         String selectUserQuery = "\n" +
                 "        SELECT u.nick as nick,\n" +
                 "            u.point as point,\n" +
@@ -28,7 +29,7 @@ public class UserDao {
                 "        GROUP BY u.nick, u.point\n";
         System.out.println(selectUserQuery);
         int selectUserParam = userId;
-        return this.jdbcTemplate.query(selectUserQuery,
+        return this.jdbcTemplate.queryForObject(selectUserQuery,
                 (rs,rowNum) -> new GetUserInfoRes(
                         rs.getString("nick"),
                         rs.getInt("point"),
@@ -42,5 +43,27 @@ public class UserDao {
                 int.class,
                 checkUserExistParams);
 
+    }
+
+    public List<GetUserPostRes> selectUserPosts(int userId){
+        String selectUserPostsQuery = "SELECT p.title as postTitle,\n" +
+                "p.point as point,\n" +
+                "COUNT(distinct r.user_id) as postResultCount,\n" +
+                "COUNT(distinct pd.post_detail_id) as qCount\n" +
+                "from post as p\n" +
+                "left join post_detail as pd on p.post_id=pd.post_id\n" +
+                "left join result as r on p.post_id=r.post_id\n" +
+                "where p.status = 'ACTIVE' " +
+                "and p.user_id=?\n" +
+                "group by pd.post_id";
+        System.out.println(selectUserPostsQuery);
+        int selectUserPostsParam = userId;
+        return this.jdbcTemplate.query(selectUserPostsQuery,
+                (rs,rowNum) -> new GetUserPostRes(
+                        rs.getString("postTitle"),
+                        rs.getInt("point"),
+                        rs.getInt("postResultCount"),
+                        rs.getInt("qCount")
+                ), selectUserPostsParam);
     }
 }
