5c7041f7ed0e5e5796f873dad6ccfc9b40fe2928
Park-EunBi <parkeb555@gmail.com>
2022-08-06 16:06:29 +0900

    ADD Get Result  API

--- src/main/java/com/springboot/moa/result/ResultProvider.java
+++ src/main/java/com/springboot/moa/result/ResultProvider.java
@@ -1,8 +1,14 @@
 package com.springboot.moa.result;
 
+import com.springboot.moa.config.BaseException;
+import com.springboot.moa.result.model.GetResultStatisticsRes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.springboot.moa.config.BaseResponseStatus.*;
 
 @Service
 public class ResultProvider {
@@ -14,4 +20,26 @@ public class ResultProvider {
     public ResultProvider(ResultDao resultDao) {
         this.resultDao = resultDao;
     }
+
+    public List<GetResultStatisticsRes> retrieveResultStatistics(long postDetailId) throws BaseException{
+        if (checkAnswerExist(postDetailId) == 0) {
+            throw new BaseException(POST_EMPTY_POST_DETAIL_ID);
+        }
+        try {
+            List<GetResultStatisticsRes> getResultStatisticsRes = resultDao.selectResult(postDetailId);
+            return getResultStatisticsRes;
+        } catch (Exception exception) {
+            throw new BaseException(DATABASE_ERROR);
+        }
+
+    }
+
+    public int checkAnswerExist(long postDetailId) throws BaseException{
+        try {
+            return resultDao.checkResultPostDetailId(postDetailId);
+        } catch (Exception exception) {
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
 }
+
