f170437e41332b1bbf3ad772f9f5944efa64a825
orzr <tjsdkfk77@naver.com>
2022-11-25 14:40:33 +0900

    modify post Url Share

---
+++ src/main/java/com/springboot/moa/post/model/GetPostUrlShareRes.java
@@ -0,0 +1,16 @@
+package com.springboot.moa.post.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class GetPostUrlShareRes {
+    String url;
+
+    public GetPostUrlShareRes() {
+    }
+}
